7d6df3acad6c52aa622788f95bad1469c22c0668
Mohith14 <86479859+Mohith14@users.noreply.github.com>
2026-09-15 12:53:02 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -0,0 +1,20 @@
+//need to return the to  k most frequent elelments in arrray 
+// we store in map and sort it 
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+
+        List<Integer> keys = new ArrayList<>(map.keySet());
+
+        keys.sort((a,b) -> map.get(b) - map.get(a));
+        int[] result = new int[k];
+        for( int i =0; i< k; i++){
+            result[i] = keys.get(i);
+        }
+        return result;
+    }
+}
